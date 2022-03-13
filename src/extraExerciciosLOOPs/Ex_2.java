7ab598df46be9858f851d53b267df1596e778289
Ragnadamm <felipe92pontes@gmail.com>
2022-03-13 18:51:36 -0300

    Add arquivos feitos até agora na máquina

---
+++ src/extraExerciciosLOOPs/Ex_2.java
@@ -0,0 +1,19 @@
+package extraExerciciosLOOPs;
+
+import java.util.Scanner;
+
+/*Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário,
+  mostrando uma mensagem de erro e voltando a pedir as informações.*/
+public class Ex_2 {
+    public static void main(String[] args) {
+        Scanner login = new Scanner(System.in);
+
+        String user;
+        String password = "PontesSETE";
+
+        System.out.println("Digite o login: ");
+        user = login.next();
+
+
+    }
+}
